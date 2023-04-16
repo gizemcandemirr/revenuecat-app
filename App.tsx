e7344e54bed4b4308883529222dc66a227a1f9f7
Gizem <gizemfener2727@gmail.com>
2023-04-16 11:43:43 +0300

    Create navigate

--- App.tsx
+++ App.tsx
@@ -1,17 +1,35 @@
-
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen
- from "./screens/HomeScreen";
-const Stack = createNativeStackNavigator();
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import DemoScreen from "./screens/DemoScreen";
+
+export type RootStackParamList = {
+  Home?: undefined;
+  Paywall?: undefined;
+  Demo?: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-        <Stack.Screen options={{
-          headerShown:false
-        }} name="Home" component={HomeScreen} />
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Demo"
+          component={DemoScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/ActionRow.tsx
+++ components/ActionRow.tsx
@@ -1,6 +1,14 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+import { useNavigation } from "@react-navigation/native";
+
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Home"
+>;
 
 type Props = {
   title: string;
@@ -19,8 +27,11 @@ const ActionRow = ({
   icon,
   vertical,
 }: Props) => {
+  const navigation = useNavigation<NavigationProp>();
+
   return (
     <TouchableOpacity
+      onPress={() => navigation.navigate(screen)}
       className={`flex flex-1 m-1 justify-center items-center py-6 rounded-lg space-x-2 ${
         vertical ? "flex-col" : "flex-row"
       }`}
